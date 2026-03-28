e392e10855e33ab80f7fa82a2154115e744a60d6
RusticTroll <willzerboy@gmail.com>
2026-03-28 14:02:44 -0400

    Added ZDoom Extended Node Vertex

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use fixed::{FixedI32, types::extra::U16};
+
 pub mod app;
 pub mod audio;
 pub mod renderer;
 pub mod wad;
+
+pub type Fixed = FixedI32<U16>;
--- src/wad/map/vertex.rs
+++ src/wad/map/vertex.rs
@@ -1,8 +1,27 @@
+use crate::Fixed;
 use bytemuck::{Pod, Zeroable};
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Pod, Zeroable)]
+/// Vanilla DOOM Vertex
 pub struct Vertex {
     pub x_position: i16,
     pub y_position: i16,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable)]
+/// ZDoom Extended Nodes Vertex
+pub struct ZVertex {
+    pub x_position: Fixed,
+    pub y_position: Fixed,
+}
+
+impl From<Vertex> for ZVertex {
+    fn from(value: Vertex) -> Self {
+        Self {
+            x_position: Fixed::from(value.x_position),
+            y_position: Fixed::from(value.y_position),
+        }
+    }
 }
